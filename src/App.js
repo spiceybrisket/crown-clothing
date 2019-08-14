65e8c740f6ea883c44fef6a871b37e1f255bac2d
Adam Spice <adamdidthis@hotmail.com>
2019-08-15 04:54:55 +1200

    added funtionaility to store user data in firestore

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
 import SigninAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
-import { auth } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
 
 
@@ -22,8 +22,21 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+
+      }
+      this.setState({ currentUser: userAuth })
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,6 +3,7 @@ import firebase from "firebase/app"
 import 'firebase/firestore'
 import 'firebase/auth'
 
+
 const config = {
   apiKey: "AIzaSyBCfbbiRodGZoRhSB5qrf49Yv0VrMKbmAA",
   authDomain: "crwn-db-14082.firebaseapp.com",
@@ -11,6 +12,32 @@ const config = {
   storageBucket: "",
   messagingSenderId: "756417892877",
   appId: "1:756417892877:web:cd640085f6e12ea6"
+}
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+  const snapShot = await userRef.get()
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+  return userRef;
+
 }
 
 
