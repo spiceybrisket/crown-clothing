26f75d68d90e318eed3a66246188fa191a184b52
Adam Spice <adamdidthis@hotmail.com>
2019-08-14 15:47:45 +1200

    Added a custom input and button component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component"
 import Header from "./components/header/header.component"
+import SigninAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
 
 
 
@@ -15,6 +16,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SigninAndSignUpPage} />
       </Switch>
     </div>
   );
