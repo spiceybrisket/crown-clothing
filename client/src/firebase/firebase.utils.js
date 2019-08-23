ad6020d1663011f42821f61ddfacf1652d053613
Adam Spice <adam@namsys.com.au>
2019-08-23 13:35:13 +1200

    typo

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14",
-  authDomain: "crwn-db.firebaseapp.com",
-  databaseURL: "https://crwn-db.firebaseio.com",
-  projectId: "crwn-db",
-  storageBucket: "crwn-db.appspot.com",
-  messagingSenderId: "850995411664",
-  appId: "1:850995411664:web:7ddc01d597846f65"
+  apiKey: "AIzaSyBCfbbiRodGZoRhSB5qrf49Yv0VrMKbmAA",
+  authDomain: "crwn-db-14082.firebaseapp.com",
+  databaseURL: "https://crwn-db-14082.firebaseio.com",
+  projectId: "crwn-db-14082",
+  storageBucket: "crwn-db-14082.appspot.com",
+  messagingSenderId: "756417892877",
+  appId: "1:756417892877:web:cd640085f6e12ea6"
 };
 
 firebase.initializeApp(config);
